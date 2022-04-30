26d730b571c4236383315fcb41a09a946be18fca
Gerry Sousa <gerry@ciandt.com>
2022-04-30 14:29:57 -0300

    Usando o RestTemplate

--- loja/src/main/java/br/com/alura/microservice/loja/loja/controller/CompraController.java
+++ loja/src/main/java/br/com/alura/microservice/loja/loja/controller/CompraController.java
@@ -1,6 +1,8 @@
 package br.com.alura.microservice.loja.loja.controller;
 
 import br.com.alura.microservice.loja.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.loja.service.CompraService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/compra")
 public class CompraController {
 
+  @Autowired
+  private CompraService compraService;
+
   @RequestMapping(method = RequestMethod.POST)
   public void realizaCompra(@RequestBody CompraDTO compra){
+    compraService.realizarCompra(compra);
 
   }
 }
---
+++ loja/src/main/java/br/com/alura/microservice/loja/loja/service/CompraService.java
@@ -0,0 +1,22 @@
+package br.com.alura.microservice.loja.loja.service;
+
+import br.com.alura.microservice.loja.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.loja.controller.dto.InfoFornecedorDTO;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class CompraService {
+
+  public void realizarCompra(CompraDTO compra) {
+    RestTemplate client = new RestTemplate();
+
+    ResponseEntity<InfoFornecedorDTO> exchange =
+        client.exchange(
+            "http://localhost:8081/info/" + compra.getEndereco().getEstado(),
+            HttpMethod.GET,
+        null,
+            InfoFornecedorDTO.class);
+    System.out.println( exchange.getBody().getEndereco());
+  }
+}
