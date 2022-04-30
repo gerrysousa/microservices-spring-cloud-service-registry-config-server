e1d6626877036b923f1363cd90920405a08b6aac
Gerry Sousa <gerry@ciandt.com>
2022-04-30 13:19:15 -0300

    Implementado o microsserviço

---
+++ loja/src/main/java/br/com/alura/microservice/loja/loja/controller/dto/EnderecoDTO.java
@@ -0,0 +1,31 @@
+package br.com.alura.microservice.loja.loja.controller.dto;
+
+public class EnderecoDTO {
+  private String rua;
+  private Integer numero;
+  private String estado;
+
+  public void setRua(String rua) {
+    this.rua = rua;
+  }
+
+  public String getRua() {
+    return rua;
+  }
+
+  public Integer getNumero() {
+    return numero;
+  }
+
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
+
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  public String getEstado() {
+    return estado;
+  }
+}
---
+++ loja/src/main/java/br/com/alura/microservice/loja/loja/controller/dto/ItemDaCompraDTO.java
@@ -0,0 +1,22 @@
+package br.com.alura.microservice.loja.loja.controller.dto;
+
+public class ItemDaCompraDTO {
+  private long id;
+  private int quantidade;
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setQuantidade(int quantidade) {
+    this.quantidade = quantidade;
+  }
+
+  public int getQuantidade() {
+    return quantidade;
+  }
+}
