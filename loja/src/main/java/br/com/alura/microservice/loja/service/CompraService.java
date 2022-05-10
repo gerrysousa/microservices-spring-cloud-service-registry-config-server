168f12a074a4a302ea08cb46a7d518efc1201267
Gerry Sousa <gerry@ciandt.com>
2022-05-10 18:36:37 -0300

    O que é Distributed Tracing?

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -5,19 +5,25 @@ import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompraService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+
   @Autowired
   private FornecedorClient fornecedorClient;
 
   public Compra realizarCompra(CompraDTO compra) {
 
+    LOG.info("buscando informaçoes do fornecedor de {}", compra.getEndereco().getEstado());
     InfoFornecedorDTO infoFornecedorDTO = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 
+    LOG.info("realizando um pedido");
     InfoPedidoDTO infoPedido = fornecedorClient.realizaPedido(compra.getItens());
     System.out.println( infoFornecedorDTO.getEndereco());
 
